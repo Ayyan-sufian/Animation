2c88d7f614803cb34468c6c2514fa6383dc473d7
Ayyan-sufian <m.ayyansufian@gmail.com>
2025-06-13 18:26:57 +0500

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +15,6 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Animation'),
@@ -28,15 +25,34 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late Animation animation;
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 4),
+    );
+    animation = Tween( begin: 0.0, end: 200.0).animate(animationController);
+    animationController.addListener((){
+      print(animation.value);
+      setState(() {
+        
+      });
+    });
+    animationController.forward();
+  }
 
   var _width = 200.0;
   var _height = 100.0;
@@ -65,72 +81,87 @@ class _MyHomePageState extends State<MyHomePage> {
               decoration: myDec,
               curve: Curves.fastOutSlowIn,
             ),
-            ElevatedButton(onPressed: (){
-              setState(() {
-               if(flag){
-                 _width = 200.0;
-                 _height = 100.0;
-                 flag = false;
-                 myDec = BoxDecoration(
-                     borderRadius: BorderRadius.circular(50),
-                   color: Colors.blue,
-                 );
-               }
-               else{
-                 _width = 100.0;
-                 _height = 200.0;
-                 flag = true;
-                 myDec = BoxDecoration(
-                     borderRadius: BorderRadius.circular(2),
-                   color: Colors.blueGrey,
-                 );
-               }
-              });
-            },
-                child: Text('Animation',style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500,),)),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  if (flag) {
+                    _width = 200.0;
+                    _height = 100.0;
+                    flag = false;
+                    myDec = BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.blue,
+                    );
+                  } else {
+                    _width = 100.0;
+                    _height = 200.0;
+                    flag = true;
+                    myDec = BoxDecoration(
+                      borderRadius: BorderRadius.circular(2),
+                      color: Colors.blueGrey,
+                    );
+                  }
+                });
+              },
+              child: Text(
+                'Animation',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+              ),
+            ),
             AnimatedOpacity(
               opacity: myOpac,
               curve: Curves.bounceInOut,
               duration: Duration(seconds: 2),
-              child: Container(
-              width: 200,
-                height: 100,
-                color: Colors.blue,
-
+              child: Container(width: 200, height: 100, color: Colors.blue),
             ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  if (isVisible) {
+                    myOpac = 0.0;
+                    isVisible = false;
+                  } else {
+                    isVisible = true;
+                    myOpac = 1.0;
+                  }
+                });
+              },
+              child: Icon(Icons.cancel),
             ),
-            ElevatedButton(onPressed: (){
-              setState(() {
-
-                if(isVisible){
-                  myOpac = 0.0;
-                  isVisible = false;
-                }
-                else{
-                  isVisible = true;
-                  myOpac = 1.0;
-                }
-              });
-            },
-                child: Icon(Icons.cancel,)),
             Center(
               child: InkWell(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => detailPg(),));
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => detailPg()),
+                  );
                 },
                 child: Container(
                   height: 100,
-                  child: Hero(tag: 'background',
-                      child: Image.asset('assets/images/rolls-royce.jpg') )
+                  child: Hero(
+                    tag: 'background',
+                    child: Image.asset('assets/images/rolls-royce.jpg'),
+                  ),
                 ),
               ),
             ),
-            ElevatedButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => listPg(),));
-            }, child: Text('3D List'))
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => listPg()),
+                );
+              },
+              child: Text('3D List'),
+            ),
+            Container(
+              width: animation.value,
+              height: animation.value,
+              color: Colors.blueGrey,
+            )
           ],
         ),
-      ) 
+      ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
